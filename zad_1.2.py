76d8df22eb11b51d4def472222e3a62bc57e564c
Bartlomiej Perelka <bpdummy82@gmail.com>
2020-05-09 00:51:19 +0200

    zad_1.2.py

--- zad_1.2.py
+++ zad_1.2.py
@@ -6,10 +6,11 @@ print("czwartek = 4")
 print("piątek = 5")
 print("sobota = 6")
 print("niedziela = 7")
-
-dzien_oddania = int(input(f"Podaj dzień tygodnia, w który oddałeś buty do szewca: "))
+print("***************")
 
 print()
+
+dzien_oddania = int(input(f"Podaj dzień tygodnia, w który oddałeś buty do szewca: "))
 
 if dzien_oddania == 1:
     print("Dzień oddania butów do szewca to poniedziałek")
@@ -28,6 +29,22 @@ elif dzien_oddania == 7:
 else:
     print("Podałeś nieprawidłową wartość")
 
-czas_naprawy = float(input(f"Podaj czas naprawy mierzony w dniach 1-7: "))
+print()
 
+czas_naprawy = float(input(f"Podaj czas naprawy mierzony w dniach: "))
+dzien_odbioru = (dzien_oddania + czas_naprawy) % 7
 
+if dzien_odbioru == 1:
+    print("Dzień odbioru butów od szewca to poniedziałek")
+elif dzien_odbioru == 2:
+    print("Dzień odbioru butów od szewca to wtorek")
+elif dzien_odbioru == 3:
+    print("Dzień odbioru butów od szewca to środa")
+elif dzien_odbioru == 4:
+    print("Dzień odbioru butów od szewca to czwartek")
+elif dzien_odbioru == 5:
+    print("Dzień odbioru butów od szewca to piątek")
+elif dzien_odbioru == 6:
+    print("Dzień odbioru butów od szewca to sobota")
+elif dzien_odbioru == 7:
+    print("Dzień odbioru butów od szewca to niedziela")
